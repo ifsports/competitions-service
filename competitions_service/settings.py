f479f3888f7966fb6e484b99c4857e63cd44a9ba
GustavoeDev <gustavoe.dev@gmail.com>
2025-07-24 11:43:56 -0300

    refactor: modify pagination number

--- competitions_service/settings.py
+++ competitions_service/settings.py
@@ -73,7 +73,7 @@ REST_FRAMEWORK = {
     'UNAUTHENTICATED_TOKEN': None,
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10, 
+    'PAGE_SIZE': 6,
 }
 
 SPECTACULAR_SETTINGS = {
